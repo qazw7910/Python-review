9c9912f5b1dcd863076443e6bcaa83b686538e51
qazw7910 <okmijn7980@gmail.com>
2022-12-10 05:08:16 +0800

    add denote

--- sort_10_method/Bubble Sort.py
+++ sort_10_method/Bubble Sort.py
@@ -3,15 +3,14 @@ def main():
     print(bubble_sort(arr))
 
 
+# time complexity : O(n**2)
 def bubble_sort(arr: list) -> list:
     for i in range(1, len(arr)):
         for j in range(0, len(arr) - i):
             if arr[j] > arr[j + 1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
     return arr
-
-
 
 
 if __name__ == '__main__':
