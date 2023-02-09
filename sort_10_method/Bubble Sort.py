33c13f8ac5b396b29cfdee4c8e7023254e926a86
qazw7910 <okmijn7980@gmail.com>
2023-02-09 13:38:29 +0800

    add calculate execute time

--- sort_10_method/Bubble Sort.py
+++ sort_10_method/Bubble Sort.py
@@ -1,12 +1,16 @@
+import time
+from typing import List
+
+
 def main():
     arr = [61, 3, 38, 5, 47, 15, 36, 26, 27, 44, 46, 39, 47, 50, 48]
     print(bubble_sort(arr))
 
 
 # time complexity : O(n**2)
-def bubble_sort(arr: list) -> list:
+def bubble_sort(arr: List[int]) -> List[int]:
     for i in range(1, len(arr)):
-        for j in range(0, len(arr) - i):
+        for j in range(len(arr) - i):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
@@ -14,4 +18,6 @@ def bubble_sort(arr: list) -> list:
 
 
 if __name__ == '__main__':
+    start_time = time.time()
     main()
+    print(time.time() - start_time)
