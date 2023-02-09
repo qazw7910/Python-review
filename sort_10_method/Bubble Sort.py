0b3057b5cb9d5c36810ae0286789a038d3b6b98a
qazw7910 <okmijn7980@gmail.com>
2023-02-09 14:32:57 +0800

    add selection_sort

--- sort_10_method/Bubble Sort.py
+++ sort_10_method/Bubble Sort.py
@@ -4,7 +4,7 @@ from typing import List
 
 def main():
     arr = [61, 3, 38, 5, 47, 15, 36, 26, 27, 44, 46, 39, 47, 50, 48]
-    print(bubble_sort(arr))
+    print(selection_sort(arr))
 
 
 # time complexity : O(n**2)
@@ -16,6 +16,16 @@ def bubble_sort(arr: List[int]) -> List[int]:
 
     return arr
 
+
+def selection_sort(arr: List[int]) -> List[int]:
+    for i in range(len(arr) - 1):
+        minIndex = i
+        for j in range(i + 1, len(arr)):
+            if arr[minIndex] > arr[j]:
+                minIndex = j
+        if i!= minIndex:
+            arr[i], arr[minIndex] = arr[minIndex], arr[i]
+    return arr
 
 if __name__ == '__main__':
     start_time = time.time()
