6c23d1d377ef8d3c2b81bb2e8b18dc50cbe5f9c4
qazw7910 <okmijn7980@gmail.com>
2023-02-09 14:39:37 +0800

    mod word form

--- sort_10_method/Bubble Sort.py
+++ sort_10_method/Bubble Sort.py
@@ -23,7 +23,7 @@ def selection_sort(arr: List[int]) -> List[int]:
         for j in range(i + 1, len(arr)):
             if arr[minIndex] > arr[j]:
                 minIndex = j
-        if i!= minIndex:
+        if i != minIndex:
             arr[i], arr[minIndex] = arr[minIndex], arr[i]
     return arr
 
