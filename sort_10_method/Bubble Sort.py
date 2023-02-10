b3da5d5aef2e303d62b6ad8a2eda07144d56d2d5
陳柏志 <okmijn7980@gmail.com>
2023-02-11 00:40:19 +0800

    insertion_sort

--- sort_10_method/Bubble Sort.py
+++ sort_10_method/Bubble Sort.py
@@ -4,7 +4,7 @@ from typing import List
 
 def main():
     arr = [61, 3, 38, 5, 47, 15, 36, 26, 27, 44, 46, 39, 47, 50, 48]
-    print(selection_sort(arr))
+    print(bubble_sort(arr))
 
 
 # time complexity : O(n**2)
@@ -13,7 +13,6 @@ def bubble_sort(arr: List[int]) -> List[int]:
         for j in range(len(arr) - i):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-
     return arr
 
 
@@ -27,7 +26,20 @@ def selection_sort(arr: List[int]) -> List[int]:
             arr[i], arr[minIndex] = arr[minIndex], arr[i]
     return arr
 
+
+# arr = [61, 3, 38, 5, 47, 15, 36, 26, 27, 44]
+def insertion_sort(arr: List[int]) -> List[int]:
+    for i in range(len(arr)):
+        preIndex = i - 1
+        current = arr[i]
+        while preIndex >= 0 and arr[preIndex] > current:
+            arr[preIndex + 1] = arr[preIndex]
+            preIndex -= 1
+        arr[preIndex + 1] = current
+    return arr
+
+
 if __name__ == '__main__':
     start_time = time.time()
     main()
-    print(time.time() - start_time)
+    print(f"{(time.time() - start_time):.10f}")
